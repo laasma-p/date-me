53a96495a4e9e1d29ce0369abfde044578aa8826
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-22 06:33:40 +0200

    Create initial component structure

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+  return (
+    <section>
+      <div>
+        <img src="https://placehold.co/400" alt="image" />
+      </div>
+      <div>
+        <h1>Title</h1>
+        <p>Paragraph 1</p>
+        <p>Paragraph 2</p>
+      </div>
+      <div>
+        <a href="#">Button 1</a>
+        <a href="#">Button 2</a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
