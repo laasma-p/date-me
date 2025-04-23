5a54a57d8dc0edf08218ceb8b7ca1bbac7128e44
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-23 21:24:58 +0200

    Style App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div>
-      <main>
+    <div className="bg-white-smoke dark:bg-nero transition-all">
+      <main className="container mx-auto px-6">
         <Hero />
       </main>
     </div>
