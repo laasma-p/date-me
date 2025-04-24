91a139d3d05f57c7f65b43283001a871a969199d
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-24 21:07:59 +0200

    Render About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="bg-white-smoke dark:bg-nero transition-all">
       <main className="container mx-auto px-6">
         <Hero />
+        <About />
       </main>
     </div>
   );
