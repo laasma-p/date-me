e0566b6e13d3873a11b25d2fa1166fca7e289964
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-24 21:07:39 +0200

    Add styling to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,12 @@ const About = () => {
   const { t } = useTranslation();
 
   return (
-    <section id="about">
-      <h2>{t("about.about-me")}</h2>
-      <p>
+    <section
+      id="about"
+      className="text-nero dark:text-white-smoke text-center max-w-4xl mx-auto py-4"
+    >
+      <h2 className="text-3xl font-semibold mb-2">{t("about.about-me")}</h2>
+      <p className="text-lg sm:text-xl">
         {t("about.description")} mais... je préférerais en apprendre plus sur
         toi. 😉
       </p>
