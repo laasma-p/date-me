c0a5bdf9318fc9da5d65963cdb1cf1d853b61adb
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-25 18:17:50 +0200

    Add styling to Interests component

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -14,32 +14,64 @@ const Interests = () => {
   const interests = t("interests.interests-list", { returnObjects: true });
 
   return (
-    <section>
-      <h2>{t("interests.interests-heading")}</h2>
-      <div>
-        <div>
-          <FontAwesomeIcon icon={faDumbbell} />
-          <span>{interests[0]}</span>
+    <section className="text-nero dark:text-white-smoke text-center max-w-4xl mx-auto py-4">
+      <h2 className="text-3xl font-semibold mb-4">
+        {t("interests.interests-heading")}
+      </h2>
+      <div className="flex flex-wrap justify-center gap-2.5">
+        <div className="flex items-center gap-2 px-4 py-2 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faDumbbell}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {interests[0]}
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faMusic} />
-          <span>{interests[1]}</span>
+        <div className="flex items-center gap-2 px-4 py-2 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faMusic}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {interests[1]}
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faPalette} />
-          <span>{interests[2]}</span>
+        <div className="flex items-center gap-2 px-4 py-2 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faPalette}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {interests[2]}
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faLaptopCode} />
-          <span>{interests[3]}</span>
+        <div className="flex items-center gap-2 px-4 py-2 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faLaptopCode}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {interests[3]}
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faTv} />
-          <span>{interests[4]}</span>
+        <div className="flex items-center gap-2 px-4 py-2 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faTv}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {interests[4]}
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faGlobe} />
-          <span>{interests[5]}</span>
+        <div className="flex items-center gap-2 px-4 py-2 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faGlobe}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {interests[5]}
+          </span>
         </div>
       </div>
     </section>
