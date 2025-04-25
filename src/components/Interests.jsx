a201d3aeb5151cb980d4fa8fa94e140b9124f567
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-25 17:59:06 +0200

    Add an icon to each respective interest

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,4 +1,13 @@
 import { useTranslation } from "react-i18next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDumbbell,
+  faMusic,
+  faPalette,
+  faLaptopCode,
+  faTv,
+  faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Interests = () => {
   const { t } = useTranslation();
@@ -9,21 +18,27 @@ const Interests = () => {
       <h2>{t("interests.interests-heading")}</h2>
       <div>
         <div>
+          <FontAwesomeIcon icon={faDumbbell} />
           <span>{interests[0]}</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faMusic} />
           <span>{interests[1]}</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faPalette} />
           <span>{interests[2]}</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faLaptopCode} />
           <span>{interests[3]}</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faTv} />
           <span>{interests[4]}</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faGlobe} />
           <span>{interests[5]}</span>
         </div>
       </div>
