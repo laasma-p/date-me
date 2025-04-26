ba8e73a808e45a3f7a05fa42d369b605eaa6cc30
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-26 12:01:36 +0200

    Add styling to IdealPartner component

--- src/components/IdealPartner.jsx
+++ src/components/IdealPartner.jsx
@@ -14,24 +14,46 @@ const IdealPartner = () => {
   });
 
   return (
-    <section>
-      <h2>{t("ideal-partner.ideal-partner-heading")}</h2>
-      <div>
-        <div>
-          <FontAwesomeIcon icon={faFaceLaugh} />
-          <span>{requirements[0]} 😀</span>
+    <section className="text-nero dark:text-white-smoke text-center max-w-4xl mx-auto py-4">
+      <h2 className="text-3xl font-semibold mb-4">
+        {t("ideal-partner.ideal-partner-heading")}
+      </h2>
+      <div className="grid sm:grid-cols-2 gap-2.5">
+        <div className="text-left flex items-center gap-4 py-3 px-5 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faFaceLaugh}
+            className="text-lg sm:text-2xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {requirements[0]} 😀
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faVenusMars} />
-          <span>{requirements[1]}</span>
+        <div className="text-left flex items-center gap-4 py-3 px-5 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faVenusMars}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {requirements[1]}
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faPaw} />
-          <span>{requirements[2]}</span>
+        <div className="text-left flex items-center gap-4 py-3 px-5 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faPaw}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {requirements[2]}
+          </span>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faHeart} />
-          <span>{requirements[3]}</span>
+        <div className="text-left flex items-center gap-4 py-3 px-5 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105">
+          <FontAwesomeIcon
+            icon={faHeart}
+            className="text-lg sm:text-xl text-mulberry dark:text-cotton-candy"
+          />
+          <span className="text-lg sm:text-xl font-semibold">
+            {requirements[3]}
+          </span>
         </div>
       </div>
     </section>
