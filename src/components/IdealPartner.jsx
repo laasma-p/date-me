bec31166bdc9ab066f92b0664eb8390fa007af90
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-26 11:55:10 +0200

    Add an icon to each respective requirement

--- src/components/IdealPartner.jsx
+++ src/components/IdealPartner.jsx
@@ -1,4 +1,11 @@
 import { useTranslation } from "react-i18next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFaceLaugh,
+  faVenusMars,
+  faPaw,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
 const IdealPartner = () => {
   const { t } = useTranslation();
@@ -11,15 +18,19 @@ const IdealPartner = () => {
       <h2>{t("ideal-partner.ideal-partner-heading")}</h2>
       <div>
         <div>
+          <FontAwesomeIcon icon={faFaceLaugh} />
           <span>{requirements[0]} 😀</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faVenusMars} />
           <span>{requirements[1]}</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faPaw} />
           <span>{requirements[2]}</span>
         </div>
         <div>
+          <FontAwesomeIcon icon={faHeart} />
           <span>{requirements[3]}</span>
         </div>
       </div>
