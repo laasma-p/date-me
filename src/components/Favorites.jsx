9bb69bb2cdae15c097a9895bd8d44630ce35e503
Lasma Poksane <laasmapoksane@gmail.com>
2025-04-26 08:42:56 +0200

    Add styling to Favorites component

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -5,14 +5,22 @@ const Favorites = () => {
   const favorites = t("favorites.favorites-list", { returnObjects: true });
 
   return (
-    <section>
-      <h2>{t("favorites.favorites-heading")}</h2>
-      <p>{t("favorites.favorites-description")}</p>
-      <div>
+    <section className="text-nero dark:text-white-smoke text-center max-w-4xl mx-auto py-4">
+      <h2 className="text-3xl font-semibold mb-2">
+        {t("favorites.favorites-heading")}
+      </h2>
+      <p className="text-lg sm:text-xl mb-4">
+        {t("favorites.favorites-description")}
+      </p>
+      <div className="grid sm:grid-cols-2 gap-2.5">
         {favorites.map(([category, item], index) => {
           return (
-            <div key={index}>
-              <span>{category}:</span> <span>{item}</span>
+            <div
+              key={index}
+              className="text-left text-lg sm:text-xl gap-2 px-4 py-2 rounded-xl border border-mulberry dark:border-cotton-candy transition-transform transform hover:scale-105"
+            >
+              <span className="font-semibold">{category}:</span>{" "}
+              <span>{item}</span>
             </div>
           );
         })}
