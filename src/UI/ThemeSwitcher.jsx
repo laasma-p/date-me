ca169f4bc708b97df4875b7ddb1192a99eb9a860
Lasma Poksane <laasmapoksane@gmail.com>
2025-05-01 15:53:17 +0200

    Add toggle the theme switch logic

--- src/UI/ThemeSwitcher.jsx
+++ src/UI/ThemeSwitcher.jsx
@@ -1,5 +1,22 @@
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 const ThemeSwitcher = () => {
-  return <button>Theme Switcher</button>;
+  const [theme, setTheme] = useState("light");
+
+  const themeToggleHandler = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+  };
+
+  return (
+    <button onClick={themeToggleHandler}>
+      <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} />
+    </button>
+  );
 };
 
 export default ThemeSwitcher;
